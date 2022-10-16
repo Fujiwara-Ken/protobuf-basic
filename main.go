006ba3c98ce9b88dd73b475de9521eabb8732f44
Fujiwara-Ken <fujiwara.k@impl.co.jp>
2022-10-17 00:50:14 +0900

    desilialize add

--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
-import "protobuf-lesson/pb"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"protobuf-lesson/pb"
 
-func main(){
-	employee := &pb.Employee {
-		Id: 1,
-		Name: "Suzuki",
-		Email: "test@example.com",
-		Occupation: pb.Occupation_ENFINEER,
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	employee := &pb.Employee{
+		Id:          1,
+		Name:        "Suzuki",
+		Email:       "test@example.com",
+		Occupation:  pb.Occupation_ENGINEER,
 		PhoneNumber: []string{"080-1234-5678", "090-1234-5678"},
-		Project: map[string]*pb.Company_Project{"ProjectX": &pb.Company_Project{}},
-		Profile: & pb.Employee_Text {
+		Project:     map[string]*pb.Company_Project{"ProjectX": &pb.Company_Project{}},
+		Profile: &pb.Employee_Text{
 			Text: "My name is Suzuki",
 		},
 		Birthday: &pb.Date{
-			Year: 2000,
+			Year:  2000,
 			Month: 1,
-			Day: 1,
+			Day:   1,
 		},
-	} 
+	}
+
+	binData, err := proto.Marshal(employee)
+	if err != nil {
+		log.Fatalln("Cant serialize", err)
+	}
+
+	if err := ioutil.WriteFile("test.bin", binData, 0666); err != nil {
+		log.Fatalln("Cant write", err)
+	}
+
+	in, err := ioutil.ReadFile("test.bin")
+	if err != nil {
+		log.Fatalln("Cant read file", err)
+	}
+
+	readEmployee := &pb.Employee{}
+
+	err = proto.Unmarshal(in, readEmployee)
+	if err != nil {
+		log.Fatalln("cant deserialize", err)
+	}
+
+	fmt.Println(readEmployee)
 }
